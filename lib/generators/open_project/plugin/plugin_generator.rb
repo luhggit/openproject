69893c974a872d1dd4e01a00b6f47ce7064864f4
Oliver Günther <mail@oliverguenther.de>
2020-07-15 15:17:10 +0200

    [33213] Fix namespace so that generator can be found
    https://community.openproject.com/wp/33213

--- lib/generators/open_project/plugin/plugin_generator.rb
+++ lib/generators/open_project/plugin/plugin_generator.rb
@@ -28,59 +28,61 @@
 
 require 'rails/generators'
 
-class Generators::OpenProject::Plugin::PluginGenerator < Rails::Generators::Base
-  source_root File.expand_path('../templates', __FILE__)
+module OpenProject
+  class PluginGenerator < Rails::Generators::Base
+    source_root File.expand_path('../templates', __FILE__)
 
-  argument :plugin_name, type: :string, default: 'openproject-new-plugin'
-  argument :root_folder, type: :string, default: 'vendor/gems'
+    argument :plugin_name, type: :string, default: 'openproject-new-plugin'
+    argument :root_folder, type: :string, default: 'vendor/gems'
 
-  # every public method is run when the generator is invoked
-  def generate_plugin
-    plugin_dir
-    lib_dir
-    bin_dir
-  end
-
-  def full_name
-    @full_name ||= begin
-      "openproject-#{plugin_name}"
+    # every public method is run when the generator is invoked
+    def generate_plugin
+      plugin_dir
+      lib_dir
+      bin_dir
     end
-  end
 
-  private
-
-  def raise_on_params
-    puts plugin_name
-    puts root_folder
-  end
-
-  def plugin_path
-    "#{root_folder}/openproject-#{plugin_name}"
-  end
-
-  def plugin_dir
-    @plugin_dir ||= begin
-      directory('', plugin_path, recursive: false)
+    def full_name
+      @full_name ||= begin
+        "openproject-#{plugin_name}"
+      end
     end
-  end
 
-  def lib_path
-    "#{plugin_path}/lib"
-  end
+    private
 
-  def lib_dir
-    @lib_dir ||= begin
-      directory('lib', lib_path)
+    def raise_on_params
+      puts plugin_name
+      puts root_folder
     end
-  end
 
-  def bin_path
-    "#{plugin_path}/bin"
-  end
+    def plugin_path
+      "#{root_folder}/openproject-#{plugin_name}"
+    end
 
-  def bin_dir
-    @bin_dir ||= begin
-      directory('bin', bin_path)
+    def plugin_dir
+      @plugin_dir ||= begin
+        directory('', plugin_path, recursive: false)
+      end
+    end
+
+    def lib_path
+      "#{plugin_path}/lib"
+    end
+
+    def lib_dir
+      @lib_dir ||= begin
+        directory('lib', lib_path)
+      end
+    end
+
+    def bin_path
+      "#{plugin_path}/bin"
+    end
+
+    def bin_dir
+      @bin_dir ||= begin
+        directory('bin', bin_path)
+      end
     end
   end
 end
